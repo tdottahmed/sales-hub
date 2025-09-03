c66592afc6fa512dc7166650671768d22cff4cc1
Shah Jalal <rjshahjalal132@gmail.com>
2025-09-03 23:48:26 +0600

    added supplier_products migration

---
+++ database/migrations/2025_09_03_174047_create_supplier_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('supplier_products', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->string('internal_id')->unique();
+            $table->longText('product_data')->nullable();
+            $table->boolean('is_processed')->default(false);
+            $table->timestamp('processed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('supplier_products');
+    }
+};
