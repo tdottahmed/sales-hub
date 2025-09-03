939955c094f444f7b0dee071e778e9dcc58324a6
Tanbir <pondit.tanbir@gmail.com>
2025-09-04 00:18:29 +0600

    add: command and service for fetch supplier product

---
+++ app/Services/FetchSupplierProduct.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services;
+
+class FetchSupplierProduct
+{
+
+    public function __construct()
+    {
+        $supplierUrl = config('credentials.supplier.base_url');
+        $apiKey = config('credentials.supplier.api_key');
+    }
+
+    public function fetch()
+    {
+
+    }
+
+    public function insert()
+    {
+
+
+    }
+}
